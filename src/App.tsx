3308d814dc9eabb29cd943223a074c20c83fb5c1
Sami Bashraheel <sami.bashraheel@gmail.com>
2026-02-03 15:41:07 +0000

    fix: correct logo path and footer text contrast

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{ duration: 0.5 }}
                     >
-                        <img src="/favicon.svg" alt="OU Calculator Logo" className="logo-icon" />
+                        <img src="./favicon.svg" alt="OU Calculator Logo" className="logo-icon" />
                     </motion.div>
                     <motion.h1
                         initial={{ opacity: 0, y: -20 }}
